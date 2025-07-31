579ecc6ad93fe3d5c0eb54a4ee92d77fbb48c0c5
SabetVar <sabetvar@proton.me>
2025-07-31 19:23:29 +0330

    Add color palette and configure Tailwind

--- new-theme/tailwind.config.js
+++ new-theme/tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
     content: [
         './**/*.php'
-    ]
+    ],
+    theme: {
+        colors: {
+            bg: 'var(--color-bg)',
+            primary: 'var(--color-primary)',
+            accent: 'var(--color-accent)',
+            secondary: 'var(--color-secondary)',
+            text: 'var(--color-text)',
+            border: 'var(--color-border)',
+            mutedBg: 'var(--color-muted-bg)',
+            transparent: 'transparent',
+            current: 'currentColor'
+        }
+    }
 };
