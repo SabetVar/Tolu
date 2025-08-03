8a10bdc0a2e23146e9a655747a0d7e934de783ea
SabetVar <sabetvar@proton.me>
2025-08-03 15:30:29 +0330

    Add Bootstrap assets and enqueue theme styles and scripts

--- wp-content/themes/toluehagh-bootstrap/functions.php
+++ wp-content/themes/toluehagh-bootstrap/functions.php
@@ -1,3 +1,10 @@
 <?php
 // Theme functions file.
 
+function toluehagh_bootstrap_enqueue_scripts() {
+    wp_enqueue_style('bootstrap', get_template_directory_uri().'/assets/bootstrap/css/bootstrap.min.css');
+    wp_enqueue_style('custom', get_template_directory_uri().'/assets/css/custom.css', array('bootstrap'));
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/assets/bootstrap/js/bootstrap.bundle.min.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'toluehagh_bootstrap_enqueue_scripts');
+
