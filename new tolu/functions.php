bb48ed165e9f4422647deda04f3f5fac822c603f
SabetVar <sabetvar@proton.me>
2025-08-03 17:45:44 +0330

    feat: rebuild header with bootstrap

--- new tolu/functions.php
+++ new tolu/functions.php
@@ -8,3 +8,33 @@ function toluehagh_bootstrap_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'toluehagh_bootstrap_enqueue_scripts');
 
+function toluehagh_bootstrap_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('custom-logo', array(
+        'height'      => 100,
+        'width'       => 300,
+        'flex-height' => true,
+        'flex-width'  => true,
+    ));
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'toluehagh-bootstrap'),
+    ));
+}
+add_action('after_setup_theme', 'toluehagh_bootstrap_setup');
+
+function toluehagh_bootstrap_nav_menu_css_class($classes, $item, $args, $depth) {
+    if (isset($args->theme_location) && 'primary' === $args->theme_location) {
+        $classes[] = 'nav-item';
+    }
+    return $classes;
+}
+add_filter('nav_menu_css_class', 'toluehagh_bootstrap_nav_menu_css_class', 10, 4);
+
+function toluehagh_bootstrap_nav_menu_link_attributes($atts, $item, $args, $depth) {
+    if (isset($args->theme_location) && 'primary' === $args->theme_location) {
+        $atts['class'] = trim(($atts['class'] ?? '') . ' nav-link');
+    }
+    return $atts;
+}
+add_filter('nav_menu_link_attributes', 'toluehagh_bootstrap_nav_menu_link_attributes', 10, 4);
+
